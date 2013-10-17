96d565d2dbe496ebb7c4082767203071f64fc615
Jeremy Bredfeldt <bredfeldt@wisc.edu>
2013-10-17 13:26:11 -0500

    Just comments

---
+++ SIM_simulation.m
@@ -0,0 +1,14 @@
+%Simulate confocal effect created by aperture coding/structured
+%illumination.
+
+%Object
+
+%PSF
+
+%Create an image without SIM
+
+%Create an image with SIM and compare
+
+
+
+%First illuminate the sample
