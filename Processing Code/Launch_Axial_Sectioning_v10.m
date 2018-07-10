b6a335a9a21a4453ead735bbf34c8867bead1c55
Niklas Gahm <niklas.gahm@gmail.com>
2018-07-10 15:22:28 -0500

    Added Axial Sectioning Launcher and all Files for Loading Microscope Configurations
    This has a minor update to the Microscope Configuration Generator to work better in accord with the Axial Sectioning GUI.
    
    Adds the finished launcher for the axial sectioning GUI (GUI will be added later)
    
    Adds the nessary sub-function to load a microscope configuration file.

---
+++ Processing Code/Launch_Axial_Sectioning_v10.m
@@ -0,0 +1,37 @@
+function [ ] = Launch_Axial_Sectioning_v10( )
+%% Axial Sectioning Launcher
+%   This launches the axial sectioning and reconstruction GUI
+% 
+%     Copyright (C) 2018 Niklas Gahm
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%   2018/07/04 - Started
+%   2018/07/04 - Finished
+
+
+%% Get Navigation Hook
+hpath = pwd;
+
+
+%% Add Required Path
+addpath('./Functions');
+
+
+%% Launch GUI Function
+Axial_Sectioning_Framework_v10_GUI; 
+
+
+%% Return to Starting Point
+cd(hpath);
+
+end
+
